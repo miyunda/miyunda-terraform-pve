c3a2c622f09ef8fbfea3676b0e53e0507d8d9145
Yu from iMAC2019 <github@beijing2b.com>
2022-12-22 11:07:44 +0800

    先弄一个看看

--- k8s-prod.tf
+++ k8s-prod.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "proxmox_vm_qemu" "k8s-prod" {
-  count       = 0
+  count       = 1
   agent       = var.pm_agent_enabled
   vmid        = 201 + count.index
   name        = "k8s-prod-0${1 + count.index}"
